df55ecb13629cc0b9eda0ffb0b50cf87bac40801
raihanonad <raihansetiawan3296@gmail.com>
2024-04-16 23:10:45 +0700

    feat add migrations and models

---
+++ server/models/rent.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Rent extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Rent.init({
+    title: DataTypes.STRING,
+    FieldId: DataTypes.INTEGER,
+    duration: DataTypes.INTEGER,
+    status: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Rent',
+  });
+  return Rent;
+};
