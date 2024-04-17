0413b823c9a303bdacbc41bf28dcc37037839c42
kemala4680 <kema20.fathan@gmail.com>
2024-04-17 17:55:22 +0700

    feat: ketinggalan

--- server/routes/reviewRoute.js
+++ server/routes/reviewRoute.js
@@ -5,6 +5,6 @@ route.get("/", ReviewController.getReview);
 route.post("/", ReviewController.createReview);
 route.get("/mine", ReviewController.getMyReview);
 route.put("/mine/:id", ReviewController.updateMyReview);
-route.delete("/mine/:id");
+route.delete("/mine/:id", ReviewController.deleteMyReview);
 
 module.exports = route;
