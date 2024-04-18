83643a6ef831f3bfebe51b50d8143df34d14b795
raihanonad <raihansetiawan3296@gmail.com>
2024-04-18 10:25:28 +0700

    fix components client

--- client/src/components/Banner.jsx
+++ client/src/components/Banner.jsx
@@ -1,5 +1,11 @@
 function Banner() {
-
+    return (
+        <>
+          <div className=" bg-red-500 w-100 h-100 border-spacing-1">
+            <h1 className="text-5xl font-semibold text-center">LET'S GO FUTSAL</h1>
+          </div>
+        </>
+      );
 }
 
 export default Banner;
--- client/src/components/CardNews.jsx
+++ client/src/components/CardNews.jsx
@@ -1,5 +1,29 @@
-function CardNews() {
+import { Link } from "react-router-dom";
 
+function CardNews({item}) {
+    return (
+        <>
+          <div className="card w-70 h-70 bg-base-100 shadow-xl rounded-lg ">
+            <figure>
+              <img className="rounded-lg"
+                src={item.url}
+                alt="Shoes"
+              />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title font-semibold">
+                {item.name}
+                <div className="badge badge-secondary"></div>
+              </h2>
+              <p>{item.description}</p>
+                <Link to={`/detail/${item.id}`}><button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded content-center mb-2 ml-1">Info</button></Link>
+              <div className="card-actions justify-end">
+                
+              </div>
+            </div>
+          </div>
+        </>
+      );
 }
 
 export default CardNews;
